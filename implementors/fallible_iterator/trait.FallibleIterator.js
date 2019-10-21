082d34464dfd973a5f0f1681ca6f7bf9c81f190e
travis <travis>
2019-10-21 15:32:38 +0000

    Automatic update for 33f930a42a23ebeace196e02b2464243fc4116cc

---
+++ implementors/fallible_iterator/trait.FallibleIterator.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["fallible_iterator"] = [];
+implementors["openstack"] = [{text:"impl&lt;Q&gt; <a class=\"trait\" href=\"fallible_iterator/trait.FallibleIterator.html\" title=\"trait fallible_iterator::FallibleIterator\">FallibleIterator</a> for <a class=\"struct\" href=\"openstack/common/struct.ResourceIterator.html\" title=\"struct openstack::common::ResourceIterator\">ResourceIterator</a>&lt;Q&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Q: <a class=\"trait\" href=\"openstack/common/trait.ResourceQuery.html\" title=\"trait openstack::common::ResourceQuery\">ResourceQuery</a>,&nbsp;</span>",synthetic:false,types:["openstack::common::resourceiterator::ResourceIterator"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ openstack/object_storage/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Container","Structure representing a single container."],["ContainerQuery","A query to containers."],["Object","Structure representing an object."],["ObjectQuery","A query to objects."]]});
